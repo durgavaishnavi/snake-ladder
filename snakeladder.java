1fdd08d17d0c3ea4b3516ada2b1771dca31e158d
Durga Vaishnavi <durgavaishnavisb@gmail.com>
2021-12-10 20:52:42 -0800

    ADDED 1st usecase

--- snakeladder.java
+++ snakeladder.java
@@ -1,6 +1,11 @@
+import java.util.*;
 public class snakeladder{
 	public static void main(String[] args){
 		System.out.println("Welcome to the game SNAKE_LADDER......");
-
+		char input;
+		System.out.println("Press 'S' to Start the game");
+		Scanner s =new Scanner(System.in);
+		input=s.next().charAt(0);
+		System.out.println("Hello Player you are at position -- 0 --");
 }
 }
